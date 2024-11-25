7c6aa25e897e4318e2e71d4a8b21bedf9c3050b1
kedama <uesastorage3@gmail.com>
2024-11-25 22:58:50 +0900

    format

--- nanoedit.ui/src/components/Main.vue
+++ nanoedit.ui/src/components/Main.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, reactive, onMounted, nextTick } from "vue";
+import { ref, reactive, onMounted, nextTick } from "vue";
 import * as monaco from "monaco-editor";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker&inline";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker&inline";
@@ -67,24 +67,28 @@ function foo() {
   */
 
   formatVue = async (srcText) => {
-    return await ps.format(srcText, { 
-      parser: "vue", 
-      plugins, 
-      tabWidth: 2 });
+    return await ps.format(srcText, {
+      parser: "vue",
+      plugins,
+      tabWidth: 2,
+    });
   };
 
   formatJs = async (srcText) => {
-    return await ps.format(srcText, { 
-      parser: "babel", 
-      plugins, 
-      tabWidth: 2 });
+    return await ps.format(srcText, {
+      parser: "babel",
+      plugins,
+      tabWidth: 2,
+    });
   };
 })();
 
-
-
-
-function createMonacoEditor(targetElement, onChange, language, initialValue = null) {
+function createMonacoEditor(
+  targetElement,
+  onChange,
+  language,
+  initialValue = null,
+) {
   let editor = monaco.editor.create(targetElement, {
     value: initialValue != null ? initialValue : "",
     language: language,
@@ -100,31 +104,35 @@ function createMonacoEditor(targetElement, onChange, language, initialValue = nu
   // editor.getModel().onDidChangeContent((event) => {
   //   onChange(editor, event);
   // });
-  
+
   // setup formatter
   setTimeout(() => {
     console.log("vue formatter loaded!");
-    monaco.languages.registerDocumentFormattingEditProvider('html', {
+    monaco.languages.registerDocumentFormattingEditProvider("html", {
       async provideDocumentFormattingEdits(model) {
         let text = await formatVue(model.getValue());
-        return [{
-          range: model.getFullModelRange(),
-          text
-        }];
-      }
+        return [
+          {
+            range: model.getFullModelRange(),
+            text,
+          },
+        ];
+      },
     });
   }, 1000);
 
   setTimeout(() => {
     console.log("js formatter loaded!");
-    monaco.languages.registerDocumentFormattingEditProvider('javascript', {
+    monaco.languages.registerDocumentFormattingEditProvider("javascript", {
       async provideDocumentFormattingEdits(model) {
         let text = await formatJs(model.getValue());
-        return [{
-          range: model.getFullModelRange(),
-          text
-        }];
-      }
+        return [
+          {
+            range: model.getFullModelRange(),
+            text,
+          },
+        ];
+      },
     });
   }, 1000);
 
@@ -145,12 +153,12 @@ onMounted(() => {
 
 function languageByExtension(extension) {
   let langs = {
-    "js" : "javascript",
-    "html" : "html",
-    "vue" : "html",
-    "css" : "css",
-    "txt" : "txt",
-  }
+    js: "javascript",
+    html: "html",
+    vue: "html",
+    css: "css",
+    txt: "txt",
+  };
   return langs[extension];
 }
 
@@ -199,22 +207,20 @@ async function openDirectory() {
   // }
 }
 
-
-
 async function deleteEntry() {
-  if(!editor.entry) return;
-  if(!editor.entry.file) return;
-  if(!editor.entry.parent) return;
+  if (!editor.entry) return;
+  if (!editor.entry.file) return;
+  if (!editor.entry.parent) return;
 
-  if(!window.confirm("Delete opened file, are you sure?")) return;
+  if (!window.confirm("Delete opened file, are you sure?")) return;
 
   // editor.entry.parent.removeEntry(editor.entry.name);
   editor.entry.handle.remove();
 }
 
 async function createNewFile(filename, parentItem) {
-  if(!parentItem) return;
-  if(parentItem.file) return;
+  if (!parentItem) return;
+  if (parentItem.file) return;
 
   let hfile = await parentItem.handle.getFileHandle(filename, { create: true });
   let writableStream = await hfile.createWritable();
@@ -248,7 +254,7 @@ async function expandTree(item) {
   let subtree = [];
   let subtreeDirs = [];
   for await (let [name, handle] of item.handle) {
-    let addTarget = (handle.kind === "file")? subtree : subtreeDirs;
+    let addTarget = handle.kind === "file" ? subtree : subtreeDirs;
     addTarget.push({
       name: name,
       handle: handle,
@@ -259,11 +265,11 @@ async function expandTree(item) {
     });
   }
 
-  subtree.sort((a,b) => {
-    return (a.name === b.name)? 0 : ((a.name > b.name)? 1 : -1);
+  subtree.sort((a, b) => {
+    return a.name === b.name ? 0 : a.name > b.name ? 1 : -1;
   });
-  subtreeDirs.sort((a,b) => {
-    return (a.name === b.name)? 0 : ((a.name > b.name)? 1 : -1);
+  subtreeDirs.sort((a, b) => {
+    return a.name === b.name ? 0 : a.name > b.name ? 1 : -1;
   });
 
   treelist.value.splice(idx + 1, 0, ...[...subtreeDirs, ...subtree]);
@@ -275,15 +281,15 @@ function prompt() {
 }
 
 async function foldTree(item) {
-  if(item.file) return;
-  let children = treelist.value.filter(i => i.parent == item);
+  if (item.file) return;
+  let children = treelist.value.filter((i) => i.parent == item);
   children.forEach(foldTree);
-  treelist.value = treelist.value.filter(i => i.parent != item);
+  treelist.value = treelist.value.filter((i) => i.parent != item);
   item.expand = false;
 }
 
 async function toggleTreeExpand(item) {
-  if(item.expand) {
+  if (item.expand) {
     foldTree(item);
   } else {
     expandTree(item);
@@ -293,18 +299,17 @@ async function toggleTreeExpand(item) {
 const selectedItem = ref(null);
 
 async function clickTreeItem(item) {
-  if(item.file) {
-    openFile(item)
+  if (item.file) {
+    openFile(item);
   }
 
   selectedItem.value = item;
 }
 
-
 function setSaveTimeout(tabitem, force = false) {
-  if(!autosaveEnabled.value && !force) return;
-  
-  if(tabitem.timeoutSave) {
+  if (!autosaveEnabled.value && !force) return;
+
+  if (tabitem.timeoutSave) {
     clearTimeout(tabitem.timeoutSave);
   }
 
@@ -325,26 +330,25 @@ const tabs = reactive({
     // { entry: { name: "index.html", hfile: null, }, preview: false, },
     // { entry: { name: "vite.svg", hfile: null, }, preview: false, },
   ],
-  selected: null //tabfirst,
+  selected: null, //tabfirst,
 });
 
 async function addTab(item) {
-
-
   const file = await item.handle.getFile();
   const reader = new FileReader();
   reader.addEventListener("load", () => {
     // editor.text = reader.result;
     let ext = item.name.split(".");
-    if(ext.length > 0 && languageByExtension(ext.slice(-1)[0])) {
+    if (ext.length > 0 && languageByExtension(ext.slice(-1)[0])) {
       var model = editorMonacoObj.getModel();
-      monaco.editor.setModelLanguage(model, languageByExtension(ext.slice(-1)[0]));
+      monaco.editor.setModelLanguage(
+        model,
+        languageByExtension(ext.slice(-1)[0]),
+      );
     }
     editorMonacoObj.setValue(reader.result);
   });
   reader.readAsText(file);
-
-
 }
 
 async function openFile(item) {
@@ -352,10 +356,10 @@ async function openFile(item) {
   //editor.name = item.name;
   //editor.entry = item;
 
-  let existing = tabs.tabitems.filter(t => t.entry == item);
-  if(existing.length > 0) {
+  let existing = tabs.tabitems.filter((t) => t.entry == item);
+  if (existing.length > 0) {
     tabs.selected = existing[0];
-    return; 
+    return;
   }
 
   let tabitem = {
@@ -376,10 +380,10 @@ async function openFile(item) {
       // editor.text = reader.result;
 
       let ext = item.name.split(".");
-      
+
       let language = "text";
 
-      if(ext.length > 0 && languageByExtension(ext.slice(-1)[0])) {
+      if (ext.length > 0 && languageByExtension(ext.slice(-1)[0])) {
         // var model = editor.getModel();
         // monaco.editor.setModelLanguage(model, languageByExtension(ext.slice(-1)[0]));
         language = languageByExtension(ext.slice(-1)[0]);
@@ -391,22 +395,20 @@ async function openFile(item) {
         (ed, val) => {
           ti.modified = true;
           setSaveTimeout(tabitem);
-        }, 
-        language);
+        },
+        language,
+      );
       tabitem.editor = editor;
 
       editor.setValue(reader.result);
-
-
 
       // add listener
       editor.getModel().onDidChangeContent((event) => {
         ((ed, val) => {
           ti.modified = true;
           setSaveTimeout(tabitem);
-        })(editor, event)
+        })(editor, event);
       });
-      
     });
     reader.readAsText(file);
   });
@@ -418,18 +420,17 @@ function clickTab(tabitem) {
 
 function deleteTab(tabitem) {
   let idx = tabs.tabitems.indexOf(tabitem);
-  tabs.tabitems = tabs.tabitems.filter(t => t != tabitem);
+  tabs.tabitems = tabs.tabitems.filter((t) => t != tabitem);
   nextTick(() => {
     // tabitem.editor.getModel().dispose();
     // tabitem.editor.dispose();
-    if(idx > 0) {
+    if (idx > 0) {
       tabs.selected = tabs.tabitems[idx - 1];
-    } else if(tabs.tabitems.length > 0) {
+    } else if (tabs.tabitems.length > 0) {
       tabs.selected = tabs.tabitems[0];
     }
-  })
+  });
 }
-
 </script>
 
 <template>
@@ -437,31 +438,60 @@ function deleteTab(tabitem) {
     <div class="tree child w10 p1 bc-gray bl bt bb flex-col">
       <div class="child flex-row">
         <span class="clickable button1" @click="openDirectory">OPEN</span>
-        <span class="clickable button1 ml1" @click="createNewFile(prompt(), selectedItem)">NEW</span>
+        <span
+          class="clickable button1 ml1"
+          @click="createNewFile(prompt(), selectedItem)"
+          >NEW</span
+        >
         <span class="clickable button1 ml1" @click="deleteEntry">DEL</span>
       </div>
       <div>
-        <label><input type="checkbox" v-model="autosaveEnabled"/>autosave</label>
+        <label
+          ><input type="checkbox" v-model="autosaveEnabled" />autosave</label
+        >
       </div>
       <br />
-      <div v-for="item in treelist" class="clickable" @click="clickTreeItem(item)">
-
-        <span style="opacity:0.3">{{ "&nbsp;|&nbsp;".repeat(item.level) }}</span>
-        <span v-if="!item.file" class="folder"
-          @click.stop="toggleTreeExpand(item)">
-          {{ (item.expand)? "-" : "+" }}&nbsp;
+      <div
+        v-for="item in treelist"
+        class="clickable"
+        @click="clickTreeItem(item)"
+      >
+        <span style="opacity:0.3">{{
+          "&nbsp;|&nbsp;".repeat(item.level)
+        }}</span>
+        <span
+          v-if="!item.file"
+          class="folder"
+          @click.stop="toggleTreeExpand(item)"
+        >
+          {{ item.expand ? "-" : "+" }}&nbsp;
         </span>
-        <span :style="{ textDecoration: (selectedItem == item)? 'underline' : '' }">&nbsp;{{ item.name }}</span>
+        <span
+          :style="{ textDecoration: selectedItem == item ? 'underline' : '' }"
+          >&nbsp;{{ item.name }}</span
+        >
         {{ item.file ? "" : "/" }}
       </div>
     </div>
     <div class="editor child w20 p1 bc-gray bl bt bb br flex-col">
       <div class="child flex-row">
-
-        <div :class="['tabitem', 'clickable', 'br', 'bc-gray', (tabitem != tabs.selected)? 'bb' : '' ]"
-          v-for="tabitem in tabs.tabitems" @click="clickTab(tabitem)">
-          <span class="tabname">{{ (tabitem.modified)? "*" : "" }}{{ tabitem.entry.name }}</span>
-          <span v-if="tabs.selected == tabitem" @click.stop="deleteTab(tabitem)">X</span>
+        <div
+          :class="[
+            'tabitem',
+            'clickable',
+            'br',
+            'bc-gray',
+            tabitem != tabs.selected ? 'bb' : '',
+          ]"
+          v-for="tabitem in tabs.tabitems"
+          @click="clickTab(tabitem)"
+        >
+          <span class="tabname"
+            >{{ tabitem.modified ? "*" : "" }}{{ tabitem.entry.name }}</span
+          >
+          <span v-if="tabs.selected == tabitem" @click.stop="deleteTab(tabitem)"
+            >X</span
+          >
         </div>
         <!--
         <div class="tabitem br bb bc-gray">index.html</div>
@@ -469,155 +499,160 @@ function deleteTab(tabitem) {
         -->
       </div>
       <template v-for="tabitem in tabs.tabitems" :key="tabitem.oid">
-        <div :id="`editor-${tabitem.oid}`"
-          :style="{width:`100%`, height:`calc(100% - 1.5em)`, display:(tabitem == tabs.selected)? `inherit` : `none`}"
-          @keydown.ctrl.s.prevent.stop="setSaveTimeout(tabitem, true)">
-        </div>
+        <div
+          :id="`editor-${tabitem.oid}`"
+          :style="{
+            width: `100%`,
+            height: `calc(100% - 1.5em)`,
+            display: tabitem == tabs.selected ? `inherit` : `none`,
+          }"
+          @keydown.ctrl.s.prevent.stop="setSaveTimeout(tabitem, true)"
+        ></div>
       </template>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .root {
-    width: 100%;
-    height: 100%;
-  }
+.root {
+  width: 100%;
+  height: 100%;
+}
 
-  .container {
-    display: flex;
-  }
+.container {
+  display: flex;
+}
 
-  .child {
-    display: flex;
-  }
+.child {
+  display: flex;
+}
 
-  .w10 {
-    width: 10em;
-  }
+.w10 {
+  width: 10em;
+}
 
-  .w20 {
-    width: 20em;
-  }
+.w20 {
+  width: 20em;
+}
 
-  .h10 {
-    height: 10em;
-  }
+.h10 {
+  height: 10em;
+}
 
-  .h20 {
-    height: 20em;
-  }
+.h20 {
+  height: 20em;
+}
 
-  .p1 {
-    padding: 1em;
-  }
+.p1 {
+  padding: 1em;
+}
 
-  .bg-red {
-    background-color: red;
-  }
+.bg-red {
+  background-color: red;
+}
 
-  .bc-light {
-    border-color: #ccc !important;
-  }
+.bc-light {
+  border-color: #ccc !important;
+}
 
-  .bc-gray {
-    border-color: #888 !important;
-  }
+.bc-gray {
+  border-color: #888 !important;
+}
 
-  .bl {
-    border-left: solid 1px;
-  }
+.bl {
+  border-left: solid 1px;
+}
 
-  .br {
-    border-right: solid 1px;
-  }
+.br {
+  border-right: solid 1px;
+}
 
-  .bt {
-    border-top: solid 1px;
-  }
+.bt {
+  border-top: solid 1px;
+}
 
-  .bb {
-    border-bottom: solid 1px;
-  }
+.bb {
+  border-bottom: solid 1px;
+}
 
-  .flex-row {
-    flex-direction: row;
-  }
+.flex-row {
+  flex-direction: row;
+}
 
-  .flex-col {
-    flex-direction: column;
-  }
+.flex-col {
+  flex-direction: column;
+}
 
-  .tree {
-    width: v-bind(cssExplorerWidth);
-    /* font-family: monospace; */
-    overflow: auto;
-    white-space: pre;
-  }
+.tree {
+  width: v-bind(cssExplorerWidth);
+  /* font-family: monospace; */
+  overflow: auto;
+  white-space: pre;
+}
 
-  .editor {
-    width: v-bind(cssEditorWidth);
-    padding: 0;
-  }
+.editor {
+  width: v-bind(cssEditorWidth);
+  padding: 0;
+}
 
-  .editor-textarea {
-    width: 100%;
-    height: 100%;
-    border: none;
-    background-color: #333;
-    resize: none;
-    outline: none;
-    white-space: pre;
-    font-size: 0.95em;
-    line-height: 1.2em;
-  }
+.editor-textarea {
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: #333;
+  resize: none;
+  outline: none;
+  white-space: pre;
+  font-size: 0.95em;
+  line-height: 1.2em;
+}
 
-  .clickable {
-    cursor: pointer;
-  }
+.clickable {
+  cursor: pointer;
+}
 
-  .button1 {
-    display: inline-block;
-    min-width: 3em;
-    background-color: #444;
-    text-align: center;
-    vertical-align: middle;
-    font-size: 0.9em;
-    padding: 3px;
-  }
+.button1 {
+  display: inline-block;
+  min-width: 3em;
+  background-color: #444;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 0.9em;
+  padding: 3px;
+}
 
-  .ml1 {
-    margin-left: 1em;
-  }
+.ml1 {
+  margin-left: 1em;
+}
 
-  .mr1 {
-    margin-right: 1em;
-  }
+.mr1 {
+  margin-right: 1em;
+}
 
-  .mt1 {
-    margin-top: 1em;
-  }
+.mt1 {
+  margin-top: 1em;
+}
 
-  .mb1 {
-    margin-bottom: 1em;
-  }
+.mb1 {
+  margin-bottom: 1em;
+}
 
-  .folder {
-    display: inline-block;
-    width: 1em;
-    text-align: center;
-  }
+.folder {
+  display: inline-block;
+  width: 1em;
+  text-align: center;
+}
 
-  .tabitem {
-    display: inline-block;
-    height: 1.5em;
-    text-align: center;
-    vertical-align: middle;
-    box-sizing: border-box;
-    padding: 0 1em;
-  }
+.tabitem {
+  display: inline-block;
+  height: 1.5em;
+  text-align: center;
+  vertical-align: middle;
+  box-sizing: border-box;
+  padding: 0 1em;
+}
 
-  .tabname {
-    padding-right: 0.5em;
-  }
+.tabname {
+  padding-right: 0.5em;
+}
 </style>
