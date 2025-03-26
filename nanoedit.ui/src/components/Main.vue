aeceb835fb688a37a7cc5c54bfdd4d29f8db3e87
kedama <uesastorage3@gmail.com>
2025-03-27 07:30:08 +0900

    session

--- nanoedit.ui/src/components/Main.vue
+++ nanoedit.ui/src/components/Main.vue
@@ -12,7 +12,7 @@ import prettierPluginHtml from "prettier/plugins/html";
 import prettierPluginEstree from "prettier/plugins/estree";
 import prettierPluginBabel from "prettier/plugins/babel";
 import storageUtil from "../storage-util.js";
-import { get, set } from 'idb-keyval';
+import { get, set } from "idb-keyval";
 
 const fontSize = ref(storageUtil.getStorage("fontSize", 13));
 
@@ -190,8 +190,11 @@ async function openDirectory() {
   // if(lastHdir && confirm('Open last directory?')) {
   //   hdir.value = lastHdir;
   // } else {
-    hdir.value = await window.showDirectoryPicker({ mode: "readwrite" });
-    await set('directory', hdir.value);
+  hdir.value = await window.showDirectoryPicker({ mode: "readwrite" });
+  await set("directory", hdir.value);
+  if(currentSession.value) {
+    currentSession.value.dir = hdir.value;
+  }
   // }
 
   treelist.value = [];
@@ -220,8 +223,8 @@ async function openDirectory() {
 }
 
 async function openLastDirectory() {
-  const lastHdir = await get('directory');
-  if(lastHdir) {
+  const lastHdir = await get("directory");
+  if (lastHdir) {
     hdir.value = lastHdir;
   } else {
     return;
@@ -252,8 +255,8 @@ async function openLastDirectory() {
   // }
 }
 
-window.debugDirectoryHandle = async () => { 
-  return await get('directory');
+window.debugDirectoryHandle = async () => {
+  return await get("directory");
 };
 
 async function deleteEntry() {
@@ -505,6 +508,62 @@ function openConfig() {
 function closeConfig() {
   showConfig.value = false;
 }
+
+const currentSession = ref(null);
+
+async function saveSession() {
+  let sessions = await get("sessions");
+  if (!sessions) {
+    sessions = [];
+  }
+  if (currentSession.value == null) {
+    let sessionName = prompt();
+    if (sessionName === undefined || sessionName === "") return;
+    sessions.push({
+      id: uuid4(),
+      name: sessionName,
+      dir: hdir.value,
+    });
+  } else {
+    let session = sessions.filter((s) => s.id === currentSession.value.id)[0];
+    session.dir = hdir.value;
+  }
+  await set("sessions", sessions);
+
+  getSessionList();
+}
+
+let sessionList = ref([]);
+
+async function getSessionList() {
+  let sessions = await get("sessions");
+  sessionList.value = sessions;
+}
+
+getSessionList();
+
+function openSession(session) {
+  const lastHdir = session.dir;
+  hdir.value = lastHdir;
+  treelist.value = [];
+  let workspace = {
+    name: "workspace",
+    handle: hdir.value,
+    file: false,
+    level: 0,
+    parent: null,
+    expand: false,
+  };
+  treelist.value.push(workspace);
+
+  expandTree(workspace);
+
+  currentSession.value = session;
+}
+
+window.iset = set;
+window.iget = get;
+window.saveSession = saveSession;
 </script>
 
 <template>
@@ -623,6 +682,22 @@ function closeConfig() {
               />
             </svg>
           </span>
+
+          <span
+            class="clickable ml1"
+            @click="saveSession"
+            v-if="cssExplorerWidth === '20em'"
+          >
+            <svg fill="#888" height="24" width="24" viewBox="0 0 72 64">
+              <path
+                d="M55.818,21.578c-0.118-0.362-0.431-0.626-0.808-0.681L36.92,18.268L28.83,1.876c-0.168-0.342-0.516-0.558-0.896-0.558
+	s-0.729,0.216-0.896,0.558l-8.091,16.393l-18.09,2.629c-0.377,0.055-0.689,0.318-0.808,0.681c-0.117,0.361-0.02,0.759,0.253,1.024
+	l13.091,12.76l-3.091,18.018c-0.064,0.375,0.09,0.754,0.397,0.978c0.309,0.226,0.718,0.255,1.053,0.076l16.182-8.506l16.18,8.506
+	c0.146,0.077,0.307,0.115,0.466,0.115c0.207,0,0.413-0.064,0.588-0.191c0.308-0.224,0.462-0.603,0.397-0.978l-3.09-18.017
+	l13.091-12.761C55.838,22.336,55.936,21.939,55.818,21.578z"
+              />
+            </svg>
+          </span>
         </div>
       </div>
       <br />
@@ -685,6 +760,23 @@ function closeConfig() {
           }"
           @keydown.ctrl.s.prevent.stop="setSaveTimeout(tabitem, true)"
         ></div>
+      </template>
+      <template v-if="tabs.tabitems.length === 0">
+        <div
+          :style="{
+            width: `100%`,
+            height: `calc(100% - 1.5em)`,
+          }"
+        >
+          <div style="margin:1.5em">
+            <p>sessions</p>
+            <ul>
+              <template v-for="session in sessionList">
+                <li @click="openSession(session)">{{ session.name }}</li>
+              </template>
+            </ul>
+          </div>
+        </div>
       </template>
     </div>
   </div>
